2c66cf00b2beae79d71d7e93e5df59a1bc5c2ee0
armanuralbyice <armanurhossainalbyice0@gmail.com>
2024-07-12 18:21:05 +0600

    edit cors url

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express()
 app.use(express.json())
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://cp-frontend-red.vercel.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Authorization', 'Content-Type']
